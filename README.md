c8f32533ba26aedf729383a87006437444f36ea0
Kirill Varlamov <kirill@ongrid.pro>
2018-10-28 13:43:18 +0300

    Improve documentation

--- README.md
+++ README.md
@@ -28,6 +28,9 @@ and their Ethereum public key gets access to the stream for given time/episodes.
 "Ursulas" delegates decryption rights for the Subscriber converting common EDEK to Subscriber's individual EDEK' key and 
 encrypts it with Subscriber public key.
 
+This example relies on the decentralised self-incentivised Ursula network to implement the Global content sharing 
+policies and implement key Re-encyption process.
+
 The subscriber now can decrypt the individual EDEK' with its private Ethereum key then the episodes can be decrypted with
 individual EDEK'.
 
@@ -38,22 +41,58 @@ ddrm.py library implements a set of abstractions:
 * ProtectedEpisodes with granular access-control (grant) method
 * StreamPlayer - subscriber's device or program which fetches and decodes the stream.
 
+[ddrm.py](ddrm.py) is concentrated purely on interaction between Subscriber and Author (actions 1-3, 8-4 from the list below) playing
+the media stream. As underlying Ursula network we were recommended to use ["Finnegan's Wake" federated example](https://github.com/nucypher/nucypher/tree/federated)
+which illustrates the main principles of such decentralised reencryption service.
+
 ### CallFlow
-In progress
+Real-world call-flow of decentralised content marketplace could look like this
+1. Author of the stream breaks the data into objects like episodes/scenes/frames and encrypts each chunk with random 
+key EDEK. Each EDEK for each episode then gets encrypted with Author's public key.
+2. The resulting set of encrypted scenes with Author-encrypted EDEKs get stored in the distributed storage (swarm/ipfs), 
+cloud (AWS S3, GCP bucket) or get queued with 0mq/rabbitmq or plain UDP/tcp streams (depending on codec requirements).
+ and converts its raw content into encrypted object (for streaming it should be iterable set of objects).
+3. Each of the chunks gets its own policy for access control (each registered and announced over set of Ursulas)
+4. Author knows URI of the encrypted set or stream endpoint and able to publish and sell access to the media.
+5. Author publishes its proposal via DApp marketplace (metadata, labels, screenshots, descriptions, trailers) and declares
+the price for single use playing or any kind of subscribtion policy resulting in payable Smart-contract on the network
+6. The marketplace visitor sees the poster and gets interested in the content (say, blogger's live stream). It deposits
+the given amount of tokens (ERC-223 or other supporting token fallback) to the contract and reports its public key.
+7. Upon receiving the token contract it gets sinked to the author and access delegation event emitted on the contract.
+8. The event of access delegation gets enforced into Ursulas (Subscriber joins policy). The Ursulas are the decentralised
+ self-incentivised network implementing the Global search graph of policies and Re-encyption engine for access delegation.
+For details of its operation see ["Finnegan's Wake" federated example](https://github.com/nucypher/nucypher/tree/federated).
+9. Subscriber receives the stream URI (common for all subscribers) and starts retrieval. Thogh the decryption EDEK key is 
+encrypted with Author's key, Subscriber has no way to decrypt it.
+10. Subscriber requests the Ursulas network to reencrypt the EDEK for it. Regarding the Re-Encryption/Recapsulation 
+principles see nuCypher [whitepaper](https://github.com/nucypher/whitepaper/blob/master/whitepaper.pdf) and reference 
+implementations in their [repos](https://github.com/nucypher/).
+11. Ursulas find the requested object in the global map of policies and, if found, Subscriber's player receives the 
+recapsulated EDEK decryptable with its public key.
+12. Upon EDEK' receival Subscriber's player decrypts copyrighted material (scene/episode/frame) and plays it.
+14. Then Subscriber's player fetches the next episode and makes the same reencryption requests (9-12) until the end of 
+the stream or policy expiration/revocation. To make this process smooth, tha data can be retrieved and decoded in advance
+(employing standard media caching principles).
+15. After revocation/expiration of the subscription (policy) the Ursula will be unable to find the Subscriber's 
+reencryption public key in the map returning the error and Subscriber's player redirects to marketplace for subscribtion 
+renewal. 
+
 
 ### Build
+Builds the docker container with batteries included (the network of three Ursulas and ddrm.py illustrating subscription 
+and playing flow)
 ```
 docker build -t ddrm .
 ```
 
 ### Run dockerized
-
+Output of all the components redirected to the stdout. Sorry fo the messy output, ddrm.py output marked with the keywords.
 ```
 docker -it run ddrm
 ```
 
 #### Run without docker
-
+If you need to debug the components in isolation:
 ```
 cd nucypher/
 pipenv install --dev --three --skip-lock
@@ -78,7 +117,6 @@ During the exploration of NuCypher re-encryption patterns and playing with feder
 and in [Russian facebook thread](https://www.facebook.com/kirill.varlamov.12/posts/1419317941532736)
 
 Many thanks to brilliant nuCypher team and [Michael Egorov](https://github.com/michwill) personally.
-
 
 ## Authors
 
--- ddrm.py
+++ ddrm.py
@@ -16,12 +16,27 @@ import requests
 requests.packages.urllib3.disable_warnings()
 
 class RawStream(object):
+    """Raw copyrighted stream. Never exposed publicly.
+
+    Attributes:
+        raw_episodes: An ordered list of the episodes
+        author: The author character who owns the data
+    """
     class RawEpisode(object):
+        """The raw episode of the stream.
+        Can be the frame/scene of the film/stream.
+        Never exposed publicly unencrypted.
+
+        Attributes:
+            label: The metadata which helps to find the related policy on Ursulas
+            content: The raw content to be encrypted
+        """
         def __init__(self, label, content):
             self.label = label
             self.content = content
 
     def __init__(self, author):
+        """The content gets initialised as a set of ordered episodes"""
         self.author = author
         self.raw_episodes = [
             self.RawEpisode(b"Episode01", b"(C)opyrighted Content of Episode 01"),
@@ -43,7 +58,24 @@ class RawStream(object):
         ]
 
 class PublicEdekProtectedStream(object):
+    """Keeps the stream metadata and the ordered set of episodes.
+    The content of episodes is encrypted so this object can be published
+    or publicly streamed.
+
+    Attributes:
+        author_pubkey: The author pubkey to authenticate the stream on receiver
+        protected_episodes: The set of the episodes where the content is encrypted.
+    """
     class PublicEdekProtectedEpisode(object):
+        """Keeps the individual episode label, data and policy configuration.
+        Data is encrypted so it's absolutely secure to keep it public
+
+        Attributes:
+            label: public unencrypted label used in search on Ursulas
+            policy: policy on Ursula (provisione for each episode)
+            data_source: the decryption abstraction, see pyUmbral docs
+            episode_message_kit: the capsule providing the reencryption capabilities, see pyUmbral docs
+        """
         def __init__(self, label, raw_episode, author, n=3, m=2):
             self.n = n
             self.m = m
@@ -57,29 +89,39 @@ class PublicEdekProtectedStream(object):
             self.episode_message_kit, self.episode_signature = self.data_source.encapsulate_single_message(raw_episode)
 
         def grant(self, subscriber, hours=1):
+            """Access can be granted on per-subscriber per-episode basis with time limits.
+            1 hour by default which is pretty enough for streaming app"""
             policy_end_datetime = maya.now() + datetime.timedelta(hours=hours)
             self.author.grant(subscriber, self.label, m=self.m, n=self.n,
                               expiration=policy_end_datetime)
+            print("DDRM:Stream:Episode: access granted for the subscriber {} for {} hours".format(subscriber,hours))
 
     def __init__(self, raw_stream):
         self.author = raw_stream.author
         self.author_pubkey = bytes(self.author.stamp)
-        # self.data_source = DataSource(policy_pubkey_enc=policy.public_key)
-        # self.data_source_public_key = bytes(self.data_source.stamp)
         self.protected_episodes = []
         for raw_episode in raw_stream.raw_episodes:
             protected_episode = self.PublicEdekProtectedEpisode(raw_episode.label, raw_episode.content, self.author)
             self.protected_episodes.append(protected_episode)
 
     def grant_access_to_episodes(self, subscriber, hours=1, from_episode_id=0, to_episode_id=None):
+        """Grants access for the set of episodes for given time"""
         if to_episode_id == None:
             to_episode_id = len(self.protected_episodes)
         for episode in range(from_episode_id, to_episode_id):
-            print("Grant %s access to episode %s" % (subscriber, episode))
+            print("DDRM:Stream:Episode: Grant access for the subscriber {} to episode {}".format(subscriber, episode))
             self.protected_episodes[episode].grant(subscriber, hours)
 
 
 class StreamPlayer(object):
+    """The subscriber's player device or program which fetches data (episodes) by given url
+    then decrypts, decodes and plays them.
+
+        Attributes:
+            current_episode_id: The next episode to play
+            protected_stream: The protected stream (stream URI)
+            subscriber: the person for whom the EDEKs will be reencrypted
+    """
     def __init__(self, protected_stream, subscriber, author, start_from_episode_id=0):
         self.current_episode_id = start_from_episode_id
         self.protected_stream = protected_stream
@@ -87,22 +129,29 @@ class StreamPlayer(object):
         self.author = author
 
     def fetch_next_episode(self):
+        """Return the next protected episode from the stream"""
         episode_id = self.current_episode_id
         episode = self.protected_stream.protected_episodes[episode_id]
         self.current_episode_id += 1
         return episode
 
     def decrypt_and_play_next_episode(self):
+        """Decrypts and plays the next episode.
+
+                Attributes:
+                    episode_to_decrypt: The next episode to decrypt
+                    decrypted_episode: The raw content of the episode, this data to be played then discarded
+        """
         episode_to_decrypt = self.fetch_next_episode()
-        self.subscriber.join_policy(episode_to_decrypt.label,  # The label - he needs to know what data he's after.
-                                    bytes(self.author.stamp),  # To verify the signature, he'll need Alice's public key.
+        self.subscriber.join_policy(episode_to_decrypt.label,
+                                    bytes(self.author.stamp),
                                     node_list=[("localhost", 3501)]
                                     )
+        print("DDRM:Player: retrieves the episode {}".format(episode_to_decrypt.label))
         decrypted_episode = self.subscriber.retrieve(message_kit=episode_to_decrypt.episode_message_kit,
                                                 data_source=episode_to_decrypt.data_source,
                                                 alice_verifying_key=UmbralPublicKey.from_bytes(
                                                     bytes(self.author.stamp)))
-        print(decrypted_episode)
         return decrypted_episode
 
 teacher_rest_port = 3501
@@ -110,7 +159,7 @@ with open("nucypher/examples/examples-runtime-cruft/node-metadata-{}".format(tea
     f.seek(0)
     teacher_bytes = binascii.unhexlify(f.read())
 URSULA = Ursula.from_bytes(teacher_bytes, federated_only=True)
-print("Will learn from {}".format(URSULA))
+print("DDRM: Will learn from {}".format(URSULA))
 SHARED_CRUFTSPACE = "{}/nucypher/examples/examples-runtime-cruft".format(os.path.dirname(os.path.abspath(__file__)))
 CRUFTSPACE = "{}/drm".format(SHARED_CRUFTSPACE)
 CERTIFICATE_DIR = "{}/certs".format(CRUFTSPACE)
@@ -119,22 +168,33 @@ os.mkdir(CRUFTSPACE)
 os.mkdir(CERTIFICATE_DIR)
 URSULA.save_certificate_to_disk(CERTIFICATE_DIR)
 
+print("DDRM: Instantiating the stream author")
 author = Author(network_middleware=RestMiddleware(),
                 known_nodes=(URSULA,),
                 federated_only=True,
                 known_certificates_dir=CERTIFICATE_DIR,
                 )
+print("DDRM: Author instantiated {}".format(author))
 author.start_learning_loop(now=True)
+print("DDRM: Instantiating raw unprotected stream to keep in secret")
 raw_stream = RawStream(author)
+print("DDRM: Raw unprotected stream instantiated {}".format(raw_stream))
+print("DDRM: Encyphering the stream for publishing")
 protected_stream = PublicEdekProtectedStream(raw_stream)
-
+print("DDRM: Stream encrypted and published {}".format(protected_stream))
+print("DDRM: Instantiating the Subscriber")
 subscriber = Subscriber(known_nodes=(URSULA,),
                         federated_only=True,
                         known_certificates_dir=CERTIFICATE_DIR)
-
+print("DDRM: Subscriber instantiated {}".format(subscriber))
+print("DDRM: grant access Subscriber to the stream (its episodes)")
 protected_stream.grant_access_to_episodes(subscriber)
-
+print("DDRM: Subscriber was given access to the episodes")
+print("DDRM: Instantiate subscriber's player and connect to the stream")
 stream_player = StreamPlayer(protected_stream, subscriber, author)
-
+print("DDRM: Subscriber's player instantiated {}".format(stream_player))
+print("DDRM: Start stream play loop")
 for i in range(10):
-    stream_player.decrypt_and_play_next_episode()
+    print("DDRM:Player: Play the stream episode {}".format(i))
+    raw_episode = stream_player.decrypt_and_play_next_episode()
+    print("DDRM:Player: Decrypted episode is {}".format(raw_episode))
